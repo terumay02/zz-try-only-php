fbdfc3292b9c8867d552d97893406d7c772c03e9
terumay02 <40092371+terumay02@users.noreply.github.com>
2018-07-02 20:33:30 +0900

    Create product.php

---
+++ product.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>プロトタイプ</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
+    <!-- ここにコードを書いていきましょう -->
+    <header>
+      <div class="container">
+
+        <div class="header-top">
+          <h1>会社名：XYZ株式会社</h1>
+        <div class="clear"></div>
+        </div>
+
+    <div class="header-titles">
+
+      <div class="header-title">
+        <a href="index.html"><img src="https://terumay02.github.io/sozai/blue-dot.png"></a>
+        <p>ホーム</p>
+      </div>
+
+      <div class="header-title">
+        <img src="https://terumay02.github.io/sozai/%E9%87%91%E9%AD%9A.png">
+        <p>●●●とは</p>
+      </div>
+      <div class="header-title">
+        <img src="https://terumay02.github.io/sozai/blue-dot.png">
+        <p>サービス一覧</p>
+      </div>
+      <div class="header-title">
+        <img src="https://terumay02.github.io/sozai/%E9%87%91%E9%AD%9A.png">
+        <p>リンク</p>
+      </div>
+
+      </div>
+     </div>
+    </header>
+ <!--  サイド    -->
+    <div class="bodybody">
+       <div class="container">
+
+      <div class="body-left">
+
+        <div class="sidebar">
+
+        <a href="member.html"><img src="https://terumay02.github.io/sozai/take.png"></a>
+        <a href="member.html"><p>会員募集</p></a>
+        </div>
+
+        <div class="sidebar">
+        <a href="product.html"><img src="https://terumay02.github.io/sozai/take.png"></a>
+         <a href="product.html"><p>商品一覧</p></a>
+        </div>
+
+        <div class="sidebar">
+        <img src="https://terumay02.github.io/sozai/take.png">
+         <a href="#"><p>配送料</p></a>
+        </div>
+
+        <div class="sidebar">
+        <img src="https://terumay02.github.io/sozai/take.png">
+        <a href="#"><p>お問い合わせ</p></a>
+        </div>
+
+        <div class="sidebar">
+        <img src="https://terumay02.github.io/sozai/take.png">
+         <a href="#"><p>連絡先</p></a>
+        </div>
+
+
+      </div>
+      <div class="body-right">
+
+        <div class="body-title">
+          <a href="#"><img class="top-title" src="https://terumay02.github.io/sozai/03.%E6%9C%A8.png">
+          </a>
+          <p>商品一覧</p>
+        </div>
+
+      <div class="products">
+        <div class="product">
+          <img class="product-1" src="http://image.itmedia.co.jp/business/articles/1601/26/macd02_mf.jpg">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+        <div class="product">
+          <img class="product-1" src="http://sej.dga.jp/i/dispImage.php?id=78870">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+        <div class="product">
+          <img class="product-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaCPy9hHNFhMc01pSdRUSi2ApFwQXl-9WzL7BUaiCg_jxqPyqwvQ">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+        <div class="product">
+          <img class="product-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-z6oMljCjSeX8QSnGP3QqkNFAsRmXPj1D-eV9sEdus3hiHiehZA">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+        <div class="product">
+          <img class="product-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6_3Y-PFVFvGzk0BuzylyLrXRRYk7_6Qb26ZmXP40aL0S7ZD_R">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+        <div class="product">
+          <img class="product-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWD7VXp4TV4RdN6goBkqFY8vtdT1ZqbNT7P-jG7d2nEziiGQqoGA">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+        <div class="product">
+          <img class="product-1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN1LmankpdgixYR68U9heWw_eJrmBO7FlK0NEHZ6MUcfOrXsdf">
+          <p>商品名</p>
+          <p>価格：●●円</p>
+        </div>
+
+
+      </div>
+      </div>
+
+
+      </div>
+
+
+     </div>
+
+
+    <footer>
+
+    </footer>
+    <div class="clear"></div>
+  </body>
+</html>
